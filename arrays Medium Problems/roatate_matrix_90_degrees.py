05e69933d408871bfbf82e4d80b3691951e589d5
Eshaanm4964 <eshaanm4964@gmail.com>
2026-01-13 11:53:14 +0530

    Recoded the code mistake

--- arrays Medium Problems/roatate_matrix_90_degrees.py
+++ arrays Medium Problems/roatate_matrix_90_degrees.py
@@ -3,6 +3,6 @@ def rotate_matrix(matrix):
     m = len(matrix[0])
     for i in range(n):
         for j in range(i+1,n):
-            matrix[i],matrix[j] = matrix[j],matrix[i]
+            matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
     for i in range(n):
         matrix.reverse(i)
