90ef3743f41daa99457861ae592fe9f9efac55cd
Eshaanm4964 <eshaanm4964@gmail.com>
2026-01-15 12:10:15 +0530

    This is easy one

---
+++ Binary Search Easy Problems/last_occurrence_sorted_array.py
@@ -0,0 +1,16 @@
+def last_occurrence(arr,x):
+    n = len(arr)
+    low = 0
+    high = n-1
+    ans = -1
+    while low<=high:
+        mid = (low+high)//2
+        if arr[mid]==x:
+            ans = arr[mid]
+            low = mid+1
+        elif x<arr[mid]:
+            high = mid-1
+        else:
+            low = mid+1 
+    return ans
+
