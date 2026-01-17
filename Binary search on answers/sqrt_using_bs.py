5a06e0538d4ba43b562a06cc4a81575a000425bf
Eshaanm4964 <eshaanm4964@gmail.com>
2026-01-17 14:39:12 +0530

    Updated

--- Binary search on answers/sqrt_using_bs.py
+++ Binary search on answers/sqrt_using_bs.py
@@ -1,17 +1,17 @@
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        if x < 2:
-            return x
+def sqrt(x):
+    if x<2:
+        return x
+    
+    low = 1
+    high = x//2
+    ans = 0
 
-        left, right, ans = 1, x // 2, 0
-
-        while left <= right:
-            mid = (left + right) // 2
-            if mid * mid <= x:
-                ans = mid
-                left = mid + 1
-            else:
-                right = mid - 1
-        return ans
-
+    while low<=high:
+        mid = (low+high)//2
+        if mid*mid <=x:
+            ans = mid
+            left = mid+1
+        else:
+            right = mid-1
+    return ans 
 
